09c5c033ed79b29b440f7f033dcbe25124d3288b
dasproloy <p.das@hzdr.de>
2024-03-01 11:43:22 +0100

    file proloy

--- proloy.py
+++ proloy.py
@@ -1,2 +1,12 @@
 print("hello proloy")
-import numpy as np
+import magpylib as magpy
+cube1 = magpy.magnet.Cuboid(
+    magnetization=(0,0,1),
+    dimension=(1,1,1),
+)
+cube2 = magpy.magnet.Cuboid(
+    magnetization=(0,0,1),
+    dimension=(2,2,2),
+    position(2,0,0)
+)
+magpy.show(cube1,cube2)
