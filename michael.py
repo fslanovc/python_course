6b709d167e43e28d150d0d535b709c9a4dbf605b
mortner <magpylib@gmail.com>
2024-03-01 10:10:50 +0100

    addcopyfighandler

--- michael.py
+++ michael.py
@@ -1,1 +1,8 @@
-print("hello michael")
+import numpy as np
+import matplotlib.pyplot as plt
+import addcopyfighandler
+
+xs = np.linspace(0,10*np.pi,100)
+ys = np.sin(xs)
+plt.plot(xs,ys)
+plt.show()
